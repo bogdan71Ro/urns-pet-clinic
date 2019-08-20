d32505cc84b50cf447ef095862e9937a17039a5e
BOGDAN PISAI <bogdan.pisai@gmail.com>
2019-08-20 17:56:42 +0300

    BPI Petclinic Commit #3

---
+++ src/main/java/org/uranussoftware/petclinic/UrnsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package org.uranussoftware.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnsPetClinicApplication.class, args);
+	}
+
+}
