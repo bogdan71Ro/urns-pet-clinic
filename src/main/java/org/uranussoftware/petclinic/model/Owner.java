c168354cf80ef2c02fb29840d6e695c49de7a77e
BOGDAN PISAI <bogdan.pisai@gmail.com>
2019-08-22 18:38:43 +0300

    BPI Petclinic Commit #4, added POJOs. Closes #2.

---
+++ src/main/java/org/uranussoftware/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package org.uranussoftware.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/org/uranussoftware/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package org.uranussoftware.petclinic.model;
+
+public class Vet extends Person {
+}
+
