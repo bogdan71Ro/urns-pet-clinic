827018260877ebafee40b67522d7a2a99d63de21
BOGDAN PISAI <bogdan.pisai@gmail.com>
2019-11-26 15:31:34 +0200

    List all Owners on Owners index page.

--- pet-clinic-web/src/main/java/org/uranussoftware/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/uranussoftware/petclinic/controllers/OwnerController.java
@@ -1,14 +1,27 @@
 package org.uranussoftware.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.uranussoftware.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // Spring automatically wired the OwnerService
+    // no need to explicitly @Autowire it hire
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
